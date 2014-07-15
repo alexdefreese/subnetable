f488575fcdf4a80747fb556299f085cf7cdcb18e
William DeFreese <alexdefreese@gmail.com>
2014-07-14 22:56:23 -0400

    Added column model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715024306) do
+ActiveRecord::Schema.define(version: 20140715025612) do
+
+  create_table "columns", force: true do |t|
+    t.string   "title"
+    t.integer  "table_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "notes", force: true do |t|
     t.text     "content"
