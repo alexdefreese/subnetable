97cd34d48f2edef221f6a7d3b2c9e3b8786f1a28
William DeFreese <alexdefreese@gmail.com>
2014-07-14 22:58:01 -0400

    Added Item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715025612) do
+ActiveRecord::Schema.define(version: 20140715025750) do
 
   create_table "columns", force: true do |t|
     t.string   "title"
+    t.integer  "table_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", force: true do |t|
+    t.string   "name"
     t.integer  "table_id"
     t.datetime "created_at"
     t.datetime "updated_at"
