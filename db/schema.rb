cb644f9a591f4759813650474f1e0d333894469e
William DeFreese <alexdefreese@gmail.com>
2014-07-20 15:31:23 -0400

    Performed table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717010957) do
+ActiveRecord::Schema.define(version: 20140718035127) do
 
   create_table "checkoffs", force: true do |t|
     t.integer  "state"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20140717010957) do
   create_table "projects_users", force: true do |t|
     t.integer "project_id"
     t.integer "user_id"
+  end
+
+  create_table "tables", force: true do |t|
+    t.string   "name"
+    t.integer  "project_id"
+    t.integer  "item_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
