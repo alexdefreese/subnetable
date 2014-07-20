f6b2cba7803dc8f14ee8d3485a775990a514443f
William DeFreese <alexdefreese@gmail.com>
2014-07-20 15:33:05 -0400

    Adds table relationships. Closes #3
    has many items
    habtm columns
    has one note
    belongs to parent
    belongs to project

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,10 @@
 class Table < ActiveRecord::Base
+
+	has_many :items
+	has_and_belongs_to_many :columns
+	has_one :note
+
+	belongs_to :parent, class_name: "Item", foreign_key: "item_id"
+	belongs_to :project
+
 end
